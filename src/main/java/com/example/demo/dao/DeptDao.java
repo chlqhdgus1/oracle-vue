addd108ed945da13762bae94334921593a58ab26
chlqhdgus1 <chlqhdgus1@naver.com>
2020-04-16 18:59:27 +0900

    oracle-vue

---
+++ src/main/java/com/example/demo/dao/DeptDao.java
@@ -0,0 +1,26 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.db.DBManager;
+import com.example.demo.vo.DeptVo;
+
+@Repository
+public class DeptDao {
+	public List<DeptVo> selectAll(){
+		return DBManager.selectAll();
+	}
+	
+	public int insert(DeptVo d) {
+		return DBManager.insertDept(d);
+	}
+	
+	public int update(DeptVo d) {
+		return DBManager.updateDept(d);
+	}
+	public int delete(DeptVo d) {
+		return DBManager.deleteDept(d);
+	}
+}
